a0e99e23ff436f3335cc70d4e1a9b3367c22141b
Xtigyro <miroslav.hadzhiev@gmail.com>
2021-01-14 13:46:53 +0000

    fix linter errors
    Signed-off-by: Xtigyro <miroslav.hadzhiev@gmail.com>

--- scripts/check-codeowners.sh
+++ scripts/check-codeowners.sh
@@ -11,10 +11,10 @@ cat <<EOF
 
 EOF
 
-for DIR in $(ls -1 -d ./charts/*)
+for DIR in "$(ls -1 -d ./charts/*)"
 do
   FILE="$DIR/Chart.yaml"
-  DIR=$(echo $DIR | sed 's/^\.//')
-  MAINTAINERS=$(yq e '.maintainers.[].name' $FILE | sed 's/^/@/' | sort --ignore-case)
-  echo $DIR/ $MAINTAINERS
+  DIR="$(echo $DIR | sed 's/^\.//')"
+  MAINTAINERS="$(yq e '.maintainers.[].name' $FILE | sed 's/^/@/' | sort --ignore-case)"
+  echo "$DIR/ $MAINTAINERS"
 done
