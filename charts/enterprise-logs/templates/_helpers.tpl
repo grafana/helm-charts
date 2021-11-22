grafana/helm-charts

commit d75c89a164ff96f3a1b400ab59c0d2bac67896cf

Fix MinIO endpoints in the resultiung `config.yaml`
Services where incorrectly configured when using the default MinIO configuration,
which pointed to `{{ .Release.Name }}-enterprise-logs-minio` instead of `{{ .Release.Name }}-minio`.

Signed-off-by: Christian Haudum <christian.haudum@gmail.com>

charts/enterprise-logs/templates/_helpers.tpl
@@ -75,3 +75,10 @@ Create the app name of enterprise-logs clients. Defaults to the same logic as "e
 {{- end -}}
 {{- end -}}
 {{- end -}}
+
+{{/*
+Create the service endpoint including port for MinIO.
+*/}}
+{{- define "enterprise-logs.minio" -}}
+{{- printf "%s-%s.%s.svc:%s" .Release.Name "minio" .Release.Namespace (.Values.minio.service.port | toString)  -}}
+{{- end -}}

charts/enterprise-logs/templates/admin-api/deployment-admin-api.yaml
@@ -79,10 +79,10 @@ spec:
             - -config.file=/etc/loki/config/config.yaml
             {{- if .Values.minio.enabled }}
             - -admin.client.backend-type=s3
-            - -admin.client.s3.endpoint={{ .Release.Name }}-minio.{{ .Release.Namespace }}.svc:9000
+            - -admin.client.s3.endpoint={{ template "enterprise-logs.minio" . }}
             - -admin.client.s3.bucket-name=enterprise-logs-admin
-            - -admin.client.s3.access-key-id=enterprise-logs
-            - -admin.client.s3.secret-access-key=supersecret
+            - -admin.client.s3.access-key-id={{ .Values.minio.accessKey }}
+            - -admin.client.s3.secret-access-key={{ .Values.minio.secretKey }}
             - -admin.client.s3.insecure=true
             {{- end }}
             {{- range $key, $value := .Values.adminApi.extraArgs }}

charts/enterprise-logs/templates/compactor/statefulset-compactor.yaml
@@ -113,10 +113,10 @@ spec:
             - -config.file=/etc/loki/config/config.yaml
             {{- if .Values.minio.enabled }}
             - -admin.client.backend-type=s3
-            - -admin.client.s3.endpoint={{ .Release.Name }}-minio.{{ .Release.Namespace }}.svc:9000
+            - -admin.client.s3.endpoint={{ template "enterprise-logs.minio" . }}
             - -admin.client.s3.bucket-name=enterprise-logs-admin
-            - -admin.client.s3.access-key-id=enterprise-logs
-            - -admin.client.s3.secret-access-key=supersecret
+            - -admin.client.s3.access-key-id={{ .Values.minio.accessKey }}
+            - -admin.client.s3.secret-access-key={{ .Values.minio.secretKey }}
             - -admin.client.s3.insecure=true
             {{- end }}
             {{- range $key, $value := .Values.compactor.extraArgs }}

charts/enterprise-logs/templates/gateway/deployment-gateway.yaml
@@ -58,10 +58,10 @@ spec:
             - -config.file=/etc/loki/config/config.yaml
             {{- if .Values.minio.enabled }}
             - -admin.client.backend-type=s3
-            - -admin.client.s3.endpoint={{ .Release.Name }}-minio.{{ .Release.Namespace }}.svc:9000
+            - -admin.client.s3.endpoint={{ template "enterprise-logs.minio" . }}
             - -admin.client.s3.bucket-name=enterprise-logs-admin
-            - -admin.client.s3.access-key-id=enterprise-logs
-            - -admin.client.s3.secret-access-key=supersecret
+            - -admin.client.s3.access-key-id={{ .Values.minio.accessKey }}
+            - -admin.client.s3.secret-access-key={{ .Values.minio.secretKey }}
             - -admin.client.s3.insecure=true
             {{- end }}
             {{- if .Values.gateway.useDefaultProxyURLs }}

charts/enterprise-logs/templates/tokengen/job-tokengen.yaml
@@ -55,10 +55,10 @@ spec:
             - -target=tokengen
             {{- if .Values.minio.enabled }}
             - -admin.client.backend-type=s3
-            - -admin.client.s3.endpoint={{ .Release.Name }}-minio.{{ .Release.Namespace }}.svc:9000
+            - -admin.client.s3.endpoint={{ template "enterprise-logs.minio" . }}
             - -admin.client.s3.bucket-name=enterprise-logs-admin
-            - -admin.client.s3.access-key-id=enterprise-logs
-            - -admin.client.s3.secret-access-key=supersecret
+            - -admin.client.s3.access-key-id={{ .Values.minio.accessKey }}
+            - -admin.client.s3.secret-access-key={{ .Values.minio.secretKey }}
             - -admin.client.s3.insecure=true
             {{- end }}
             - -tokengen.token-file=/shared/admin-token

charts/enterprise-logs/values.yaml
@@ -17,7 +17,7 @@ image:
   # -- The image repository to use
   repository: grafana/enterprise-logs
   # -- The version of Grafana Enterprise Logs
-  tag: v1.1.0
+  tag: v1.2.0
   # -- Defines the policy how and when images are pulled
   pullPolicy: IfNotPresent
   # -- Additional image pull secrets
@@ -71,7 +71,7 @@ config:
     storage:
       type: s3
       s3:
-        endpoint: "{{ include \"loki.fullname\" . }}-minio:9000"
+        endpoint: "{{ include \"enterprise-logs.minio\" . }}"
         bucket_name: enterprise-logs-admin
         secret_access_key: supersecret
         access_key_id: enterprise-logs
@@ -147,7 +147,7 @@ config:
 
   storage_config:
     aws:
-      endpoint: "{{ include \"loki.fullname\" . }}-minio:9000"
+      endpoint: "{{ include \"enterprise-logs.minio\" . }}"
       bucketnames: enterprise-logs-tsdb
       access_key_id: enterprise-logs
       secret_access_key: supersecret
@@ -165,7 +165,7 @@ config:
     storage:
       type: s3
       s3:
-        endpoint: "{{ include \"loki.fullname\" . }}-minio:9000"
+        endpoint: "{{ include \"enterprise-logs.minio\" . }}"
         bucketnames: enterprise-logs-ruler
         access_key_id: enterprise-logs
         secret_access_key: supersecret

commit 500fb8737ae3418d507e5bda35bb0876001de6d2

Fix error in template rendering when MinIO is disabled
Commit d75c89a164ff96f3a1b400ab59c0d2bac67896cf introduced a bug that
caused an error when MinIO was disabled. It was caused by the
`"enterprise-logs.minio"` template macro, which tried to sus the service
port configuration even when `minio.enabled` was set to `false`.

Signed-off-by: Christian Haudum <christian.haudum@gmail.com>

charts/enterprise-logs/templates/_helpers.tpl
@@ -80,5 +80,7 @@ Create the app name of enterprise-logs clients. Defaults to the same logic as "e
 Create the service endpoint including port for MinIO.
 */}}
 {{- define "enterprise-logs.minio" -}}
-{{- printf "%s-%s.%s.svc:%s" .Release.Name "minio" .Release.Namespace (.Values.minio.service.port | toString)  -}}
+{{- if .Values.minio.enabled -}}
+{{- printf "%s-%s.%s.svc:%s" .Release.Name "minio" .Release.Namespace (.Values.minio.service.port | toString) -}}
+{{- end -}}
 {{- end -}}